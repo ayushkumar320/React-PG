4a172d0d01b0900490e072229db83f49bb9f2a23
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-08-03 16:27:33 +0530

    worked wth component breaking and typescript

--- hello-ts/src/App.tsx
+++ hello-ts/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Todo from "./components/Todo/index" 
+// React.FC - Functional Component type
+// JSX.Element - Type for the return value of a component
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is a Todo List</h1>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
-  );
+  )
 }
 
 export default App;
