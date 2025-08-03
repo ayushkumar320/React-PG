7a259902fee76988c4ff8cfee85a4250440edc06
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-08-03 14:47:52 +0530

    explored jsx return statement

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return <p> This is a p tag</p>
+//   <div>
+//     <h1> Custom react app </h1>
+//   </div>
+//  To return multiline JSX. we need to return in paraentheses
+//   return (
+//     <div>
+//       <h1> Custom react app </h1>
+//       <p> This is a p tag</p>
+//     </div>
+//   )
+// }
+
+
+
 function App() {
   return (
     <div className="App">
